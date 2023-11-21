553c5799b27e67b41f41c95adbeaf18928649dcc
olehmartynenko <martynenkooleg77@gmail.com>
2023-11-21 19:44:33 +0200

    Added successful test and greeted Nikita back

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("privet oleh");
+    println!("privet nikita");
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_pass() {
+        let result = 2 - 2;
+        assert_eq!(result, 0);
+    }
 }
