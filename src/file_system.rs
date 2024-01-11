eea3fd6ff0b478b22d30f86372b4b4e321ab29dc
Nesterov, Nikita (K-DAPP/4) <nikita.nesterov@volkswagen.de>
2024-01-11 14:44:54 +0100

    replace file system mock with the fake implementation

--- src/file_system.rs
+++ src/file_system.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::{File, OpenOptions},
     io::{Read, Write},
 };
@@ -32,5 +33,39 @@ impl FileSystemOperations for FileSystem {
 
         file.write_all(content.as_bytes())?;
         Ok(())
+    }
+}
+
+pub struct FakeFileSystem {
+    pub file_contents: HashMap<String, String>,
+}
+
+impl FileSystemOperations for FakeFileSystem {
+    fn exists(&self, file_path: &str) -> bool {
+        self.file_contents.contains_key(file_path)
+    }
+
+    fn read_file(&self, file_path: &str) -> Result<String, std::io::Error> {
+        match self.file_contents.get(file_path) {
+            Some(content) => Ok(content.to_string()),
+            None => Err(std::io::Error::new(
+                std::io::ErrorKind::NotFound,
+                "File not found",
+            )),
+        }
+    }
+
+    fn write_file(&mut self, file_path: &str, content: &str) -> Result<(), std::io::Error> {
+        self.file_contents
+            .insert(file_path.to_string(), content.to_string());
+        Ok(())
+    }
+}
+
+impl Default for FakeFileSystem {
+    fn default() -> Self {
+        FakeFileSystem {
+            file_contents: HashMap::new(),
+        }
     }
 }
--- tests/main_test.rs
+++ tests/main_test.rs
@@ -1,55 +1,10 @@
 use tetris::{file_system::FileSystemOperations, main_impl};
 
-struct MockFileSystem {
-    is_exist: bool,
-    file_content_result: Result<String, std::io::Error>,
-    expected_output: &'static str,
-    write_file_result: Result<(), std::io::Error>,
-}
-
-impl FileSystemOperations for MockFileSystem {
-    fn exists(&self, _file_path: &str) -> bool {
-        self.is_exist
-    }
-    fn read_file(&self, _file_path: &str) -> Result<String, std::io::Error> {
-        match self.file_content_result {
-            Err(_) => Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Couldn't read file",
-            )),
-            Ok(ref value) => Ok(value.clone()),
-        }
-    }
-    fn write_file(&mut self, _file_path: &str, content: &str) -> Result<(), std::io::Error> {
-        assert_eq!(content, self.expected_output);
-
-        match self.write_file_result {
-            Err(_) => Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Couldn't save file",
-            )),
-            Ok(_) => Ok(()),
-        }
-    }
-}
-
-impl Default for MockFileSystem {
-    fn default() -> Self {
-        MockFileSystem {
-            is_exist: false,
-            file_content_result: Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Couldn't read file",
-            )),
-            expected_output: "",
-            write_file_result: Ok(()),
-        }
-    }
-}
-
 #[cfg(test)]
 mod tests {
-    use tetris::output::FakeOutput;
+    use std::collections::HashMap;
+
+    use tetris::{file_system::FakeFileSystem, output::FakeOutput};
 
     use super::*;
 
@@ -59,7 +14,9 @@ mod tests {
 
         let mut fake_output = FakeOutput::default();
 
-        main_impl(args, &mut MockFileSystem::default(), &mut fake_output);
+        let mut fake_file_system = FakeFileSystem::default();
+
+        main_impl(args, &mut fake_file_system, &mut fake_output);
 
         assert_eq!(
             fake_output.messages[0],
@@ -71,76 +28,17 @@ mod tests {
     fn should_output_error_if_file_not_exists() {
         let args: Vec<String> = vec!["messi.txt".to_string()];
 
-        let mut mock_file_system = MockFileSystem {
-            is_exist: false,
-            ..Default::default()
-        };
+        let mut fake_file_system = FakeFileSystem::default();
 
         let mut fake_output = FakeOutput::default();
 
-        main_impl(args, &mut mock_file_system, &mut fake_output);
+        main_impl(args, &mut fake_file_system, &mut fake_output);
 
         assert_eq!(fake_output.messages[0], String::from("File not exists"));
     }
 
     #[test]
-    fn should_output_error_if_could_not_read_file() {
-        let args: Vec<String> = vec!["messi.txt".to_string()];
-
-        let mut mock_file_system = MockFileSystem {
-            file_content_result: Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Couldn't read file",
-            )),
-            is_exist: true,
-            ..Default::default()
-        };
-
-        let mut fake_output = FakeOutput::default();
-
-        main_impl(args, &mut mock_file_system, &mut fake_output);
-
-        assert_eq!(fake_output.messages[0], String::from("Couldn't read file"));
-    }
-
-    #[test]
     fn should_correctly_play_game() {
-        let input = r"3 4
-            .p.
-            pp.
-            ...
-            ###"
-        .to_string();
-
-        let output_str = "...
-.p.
-pp.
-###
-";
-
-        let args: Vec<String> = vec!["messi.txt".to_string()];
-
-        let mut mock_file_system = MockFileSystem {
-            file_content_result: Ok(input),
-            is_exist: true,
-            expected_output: output_str,
-            ..Default::default()
-        };
-
-        let mut fake_output = FakeOutput::default();
-
-        main_impl(args, &mut mock_file_system, &mut fake_output);
-
-        assert_eq!(fake_output.messages[0], "File created");
-    }
-
-    #[test]
-    fn should_display_error_message_on_file_not_saved() {
-        let output_str = "...
-.p.
-pp.
-###
-";
         let input = r"3 4
 .p.
 pp.
@@ -148,22 +46,26 @@ pp.
 ###"
         .to_string();
 
-        let args = vec!["messi.txt".to_string()];
-        let mut mock_file_system = MockFileSystem {
-            file_content_result: Ok(input),
-            is_exist: true,
-            expected_output: output_str,
-            write_file_result: Err(std::io::Error::new(
-                std::io::ErrorKind::Other,
-                "Couldn't save file",
-            )),
-            ..Default::default()
-        };
+        let output_str = "...
+.p.
+pp.
+###
+";
+
+        let args: Vec<String> = vec!["messi.txt".to_string()];
+
+        let mut file_contents = HashMap::new();
+
+        file_contents.insert("messi.txt".to_string(), input);
+
+        let mut fake_file_system = FakeFileSystem { file_contents };
 
         let mut fake_output = FakeOutput::default();
 
-        main_impl(args, &mut mock_file_system, &mut fake_output);
+        main_impl(args, &mut fake_file_system, &mut fake_output);
 
-        assert_eq!(fake_output.messages[0], "Couldn't save file");
+        assert_eq!(fake_output.messages[0], "File created");
+
+        assert_eq!(fake_file_system.read_file("out.txt").unwrap(), output_str);
     }
 }
